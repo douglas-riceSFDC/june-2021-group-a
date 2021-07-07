787acaf47f1cda0e2317d05e5f88f8a86aff69df
Adam Brunner <abrunner@salesforce.com>
2021-07-07 14:05:01 -0400

    CLENHBC-597: Add logic to set isInStock property based on Available_Stock__c field

--- force-app/main/default/lwc/titleDetails/titleDetails.js
+++ force-app/main/default/lwc/titleDetails/titleDetails.js
@@ -12,19 +12,19 @@ import POSTER_FIELD from '@salesforce/schema/Title__c.Poster__c';
 import RELEASE_DATE_FIELD from '@salesforce/schema/Title__c.Release_Date__c';
 import RUNTIME_FIELD from '@salesforce/schema/Title__c.Runtime__c';
 import WRITER_FIELD from '@salesforce/schema/Title__c.Writer__c';
+import AVAILABLE_STOCK_FIELD from '@salesforce/schema/Title__c.Available_Stock__c';
 
 const FIELDS = [ NAME_FIELD, DIRECTOR_FIELD, GENRE_FIELD, LANGUAGE_FIELD,
                  NOTABLE_ACTORS_FIELD, PLOT_DESCRIPTION_FIELD, POSTER_FIELD,
-                 RELEASE_DATE_FIELD, RUNTIME_FIELD, WRITER_FIELD ]
+                 RELEASE_DATE_FIELD, RUNTIME_FIELD, WRITER_FIELD, AVAILABLE_STOCK_FIELD ]
 
 export default class TitleDetails extends LightningElement {
 
     @api recordId;
     @api titleObject = TITLE_OBJECT;
+    @track isInStock;
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     title;
-
-    @track isInStock;
 
     get name() { return this.title.data ? getFieldValue(this.title.data, NAME_FIELD) : ''; }
     get director() { return this.title.data ? getFieldValue(this.title.data, DIRECTOR_FIELD) : '' }
@@ -36,6 +36,16 @@ export default class TitleDetails extends LightningElement {
     get releaseDate() { return this.title.data ? getFieldValue(this.title.data, RELEASE_DATE_FIELD) : '' }
     get runtime() { return this.title.data ? getFieldValue(this.title.data, RUNTIME_FIELD) : '' }
     get writer() { return this.title.data ? getFieldValue(this.title.data, WRITER_FIELD) : '' }
+    get availableStock() {
+        if (this.title.data) {
+            let stock = getFieldValue(this.title.data, AVAILABLE_STOCK_FIELD);
+            stock ? this.isInStock = true : this.isInStock = false;
+            return stock;
+        }
+        else {
+            return '';
+        }
+    }
 
     handleRentalClick() {
 
