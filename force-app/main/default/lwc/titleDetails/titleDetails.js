61d60d4344dbd2597cc1ee4050778afd95eb4351
Adam Brunner <abrunner@salesforce.com>
2021-07-07 11:36:15 -0400

    CLENHBC-597: Add record field retrieval logic to .js and metadata files

--- force-app/main/default/lwc/titleDetails/titleDetails.js
+++ force-app/main/default/lwc/titleDetails/titleDetails.js
@@ -1,8 +1,41 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+
+import TITLE_OBJECT from '@salesforce/schema/Title__c';
+import NAME_FIELD from '@salesforce/schema/Title__c.Name';
+import DIRECTOR_FIELD from '@salesforce/schema/Title__c.Director__c';
+import GENRE_FIELD from '@salesforce/schema/Title__c.Genre__c';
+import LANGUAGE_FIELD from '@salesforce/schema/Title__c.Language__c';
+import NOTABLE_ACTORS_FIELD from '@salesforce/schema/Title__c.Notable_Actors__c';
+import PLOT_DESCRIPTION_FIELD from '@salesforce/schema/Title__c.Plot_Description__c';
+import POSTER_FIELD from '@salesforce/schema/Title__c.Poster__c';
+import RELEASE_DATE_FIELD from '@salesforce/schema/Title__c.Release_Date__c';
+import RUNTIME_FIELD from '@salesforce/schema/Title__c.Runtime__c';
+import WRITER_FIELD from '@salesforce/schema/Title__c.Writer__c';
+
+const FIELDS = [ NAME_FIELD, DIRECTOR_FIELD, GENRE_FIELD, LANGUAGE_FIELD,
+                 NOTABLE_ACTORS_FIELD, PLOT_DESCRIPTION_FIELD, POSTER_FIELD,
+                 RELEASE_DATE_FIELD, RUNTIME_FIELD, WRITER_FIELD ]
 
 export default class TitleDetails extends LightningElement {
-    @track title;
-    @track isInStock = false;
+
+    @api recordId;
+    @api titleObject = TITLE_OBJECT;
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    title;
+
+    @track isInStock;
+
+    get name() { return this.title.data ? getFieldValue(this.title.data, NAME_FIELD) : ''; }
+    get director() { return this.title.data ? getFieldValue(this.title.data, DIRECTOR_FIELD) : '' }
+    get genre() { return this.title.data ? getFieldValue(this.title.data, GENRE_FIELD) : '' }
+    get language() { return this.title.data ? getFieldValue(this.title.data, LANGUAGE_FIELD) : '' }
+    get notableActors() { return this.title.data ? getFieldValue(this.title.data, NOTABLE_ACTORS_FIELD) : '' }
+    get plotDescription() { return this.title.data ? getFieldValue(this.title.data, PLOT_DESCRIPTION_FIELD) : '' }
+    get poster() { return this.title.data ? getFieldValue(this.title.data, POSTER_FIELD) : '' }
+    get releaseDate() { return this.title.data ? getFieldValue(this.title.data, RELEASE_DATE_FIELD) : '' }
+    get runtime() { return this.title.data ? getFieldValue(this.title.data, RUNTIME_FIELD) : '' }
+    get writer() { return this.title.data ? getFieldValue(this.title.data, WRITER_FIELD) : '' }
 
     handleRentalClick() {
 
