2c2ec81144397dede73bea343d3b79d5ea81c573
Adam Brunner <abrunner@salesforce.com>
2021-07-08 17:49:37 -0400

    CLENHBC-586: Add static star icons to cards and next to average
    Includes initial logic for averageRating calculation

--- force-app/main/default/lwc/reviewList/reviewList.js
+++ force-app/main/default/lwc/reviewList/reviewList.js
@@ -3,12 +3,19 @@ import { LightningElement, api, track } from 'lwc';
 
 export default class ReviewList extends LightningElement {
     @api recordId;
-    @track reviews
+    @track reviews;
+    @track averageRating;
 
     connectedCallback() {
         getTitleReviews({ titleId: this.recordId})
             .then(result => {
                 this.reviews = result;
+
+                let ratings = [];
+                for (const review in this.reviews) {
+                    ratings.push(review.Rating__c)
+                }
+                this.averageRating = ratings.reduce((total, current) => total + current) / ratings.length;
             })
             .catch(error => {
                 console.error('Error occured', error);
