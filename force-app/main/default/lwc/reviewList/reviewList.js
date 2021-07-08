e15dc5fcb480d8c8f6af5552fc6c8bc14365c754
Adam Brunner <abrunner@salesforce.com>
2021-07-08 15:04:16 -0400

    CLENHBC-586: Add initial markup and Movie Review data retrieval

--- force-app/main/default/lwc/reviewList/reviewList.js
+++ force-app/main/default/lwc/reviewList/reviewList.js
@@ -1,5 +1,21 @@
-import { LightningElement, api } from 'lwc';
+import getTitleReviews from '@salesforce/apex/TitleAuraService.getTitleReviews';
+import { LightningElement, api, track } from 'lwc';
 
 export default class ReviewList extends LightningElement {
     @api recordId;
+    @track reviews
+
+    connectedCallback() {
+        getTitleReviews({ titleId: this.recordId})
+            .then(result => {
+                this.reviews = result;
+            })
+            .catch(error => {
+                console.error('Error occured', error);
+            });
+    }
+
+    handleReviewButtonClick() {
+
+    }
 }
