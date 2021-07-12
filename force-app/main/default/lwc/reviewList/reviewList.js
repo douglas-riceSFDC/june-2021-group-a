453a8fc87a37b786e958551626726a8341d15c7a
Adam Brunner <abrunner@salesforce.com>
2021-07-12 10:27:46 -0400

    CLENHBC-585: Create reviewSubmission LWC, add to reviewList markup

--- force-app/main/default/lwc/reviewList/reviewList.js
+++ force-app/main/default/lwc/reviewList/reviewList.js
@@ -6,11 +6,12 @@ import AVERAGE_USER_RATING_FIELD from '@salesforce/schema/Title__c.Average_User_
 export default class ReviewList extends LightningElement {
     @api recordId;
     @track reviews;
-    @wire(getRecord, { recordId: '$recordId', fields: [AVERAGE_USER_RATING_FIELD] })
+    @wire(getRecord, { recordId: '$recordId', fields: AVERAGE_USER_RATING_FIELD })
     title;
 
     get averageUserRating() {
-        return this.title.data ? getFieldValue(this.title.data, AVERAGE_USER_RATING_FIELD) : 'No data';
+        console.log(this.title.data);
+        return this.title.data ? getFieldValue(this.title.data, AVERAGE_USER_RATING_FIELD) : 0 ;
     }
 
     connectedCallback() {
@@ -21,9 +22,5 @@ export default class ReviewList extends LightningElement {
             .catch(error => {
                 console.error('Error occured', error);
             });
-    }
-
-    handleReviewButtonClick() {
-
     }
 }
---
+++ force-app/main/default/lwc/reviewSubmission/reviewSubmission.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class ReviewSubmission extends LightningElement {
+
+    handleReviewButtonClick() {
+
+    }
+}
