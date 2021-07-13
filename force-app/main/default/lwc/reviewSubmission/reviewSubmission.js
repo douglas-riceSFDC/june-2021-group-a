8fa516aaa57dd12d10fc821d8586c3666fc91ed6
Adam Brunner <abrunner@salesforce.com>
2021-07-13 13:56:11 -0400

    CLENHBC-585: Convert rating input to clickable stars

--- force-app/main/default/lwc/reviewSubmission/reviewSubmission.js
+++ force-app/main/default/lwc/reviewSubmission/reviewSubmission.js
@@ -6,6 +6,12 @@ import generateMovieReview from '@salesforce/apex/MovieReviewAuraService.generat
 export default class ReviewSubmission extends LightningElement {
     @api title;
     @track isMenuOpen = false;
+    @track starState1 = "default";
+    @track starState2 = "default";
+    @track starState3 = "default";
+    @track starState4 = "default";
+    @track starState5 = "default";
+    @track selectedRating;
 
     handleReviewButtonClick() {
         this.isMenuOpen = true;
@@ -17,15 +23,14 @@ export default class ReviewSubmission extends LightningElement {
 
     handleSubmitButtonClick() {
 
-        const ratingInput = this.template.querySelector('.rating-input');
         const reviewInput = this.template.querySelector('.review-input');
 
-        if (ratingInput.validity.valid && reviewInput.validity.valid) {
+        if (this.selectedRating && reviewInput.validity.valid) {
             generateMovieReview({
                 titleId: this.title,
                 reviewerId: uId,
                 review: reviewInput.value,
-                rating: ratingInput.value
+                rating: this.selectedRating 
             })
             .then( () => {
                 location.reload();
@@ -40,13 +45,148 @@ export default class ReviewSubmission extends LightningElement {
             });
             this.isMenuOpen = false;
         }
-        else {
+        else if (!this.selectedRating && !reviewInput.validity.valid) {
             const toastEvent = new ShowToastEvent({
                 title: "Submission failed",
-                message: "Please update the invalid form entries and try again.",
+                message: "Please write a review and select a rating.",
                 variant: "error"
             });
             this.dispatchEvent(toastEvent);
+        }
+        else if (!this.selectedRating) {
+            const toastEvent = new ShowToastEvent({
+                title: "Submission failed",
+                message: "Please select a rating by clicking on the stars.",
+                variant: "error"
+            });
+            this.dispatchEvent(toastEvent);
+        }
+        else if (!reviewInput.validity.valid) {
+            const toastEvent = new ShowToastEvent({
+                title: "Submission failed",
+                message: "Please write a review in the text box.",
+                variant: "error"
+            });
+            this.dispatchEvent(toastEvent);
+        }
+        else {
+            const toastEvent = new ShowToastEvent({
+                title: "Submission failed",
+                message: "Unknown error occured",
+                variant: "error"
+            });
+            this.dispatchEvent(toastEvent);
+        }
+    }
+
+    handleRatingHover(event) {
+        switch(event.target.getAttribute('data-id')) {
+            case "star1":
+                this.starState1 = "warning";
+                this.starState2 = "default";
+                this.starState3 = "default";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+            case "star2":
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "default";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+            case "star3":
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "warning";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+            case "star4":
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "warning";
+                this.starState4 = "warning";
+                this.starState5 = "default";
+                break;
+            case "star5":
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "warning";
+                this.starState4 = "warning";
+                this.starState5 = "warning";
+                break;
+            default:
+                break;
+        }
+    }
+
+    handleRatingHoverOut() {
+        switch(this.selectedRating) {
+            case 1:
+                this.starState1 = "warning";
+                this.starState2 = "default";
+                this.starState3 = "default";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+            case 2:
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "default";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+            case 3:
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "warning";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+            case 4:
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "warning";
+                this.starState4 = "warning";
+                this.starState5 = "default";
+                break;
+            case 5:
+                this.starState1 = "warning";
+                this.starState2 = "warning";
+                this.starState3 = "warning";
+                this.starState4 = "warning";
+                this.starState5 = "warning";
+                break;
+            default:
+                this.starState1 = "default";
+                this.starState2 = "default";
+                this.starState3 = "default";
+                this.starState4 = "default";
+                this.starState5 = "default";
+                break;
+        }
+    }
+
+    handleRatingClick(event) {
+        switch(event.target.getAttribute('data-id')) {
+            case "star1":
+                this.selectedRating = 1;
+                break;
+            case "star2":
+                this.selectedRating = 2;
+                break;
+            case "star3":
+                this.selectedRating = 3;
+                break;
+            case "star4":
+                this.selectedRating = 4;
+                break;
+            case "star5":
+                this.selectedRating = 5;
+                break;
+            default:
+                break;
         }
     }
 }
