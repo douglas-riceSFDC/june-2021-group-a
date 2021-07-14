dc71c7c4b8ef8a6a77452eca77e0fd89f99559cd
adamtischler-salesforce <adam.tischler@salesforce.com>
2021-07-14 16:45:29 -0400

    HistoryPage is finished

--- force-app/main/default/lwc/rentedTitlesList/rentedTitlesList.js
+++ force-app/main/default/lwc/rentedTitlesList/rentedTitlesList.js
@@ -1,6 +1,14 @@
 import { LightningElement, wire, track } from 'lwc';
 import getCurrentRentalsForUser from '@salesforce/apex/RentalAuraService.getCurrentRentalsForUser';
+import getPastRentalsForUser from '@salesforce/apex/RentalAuraService.getPastRentalsForUser';
 
 export default class RentedTitlesList extends LightningElement {
-    @wire(getCurrentRentalsForUser) rentals;
+    @wire(getCurrentRentalsForUser) currentRentals;
+    @wire(getPastRentalsForUser) pastRentals;
+    isSelected = false;
+
+    handleClick(){
+        this.isSelected = !this.isSelected;
+    }
+
 }
--- force-app/main/default/lwc/reviewList/reviewList.js
+++ force-app/main/default/lwc/reviewList/reviewList.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
 import getTitleReviews from '@salesforce/apex/MovieReviewAuraService.getTitleReviews';
+import generateUserReviews from '@salesforce/apex/MovieReviewAuraService.generateUserReviews';
 import AVERAGE_USER_RATING_FIELD from '@salesforce/schema/Title__c.Average_User_Rating__c'
 
 export default class ReviewList extends LightningElement {
@@ -8,16 +9,30 @@ export default class ReviewList extends LightningElement {
     @track reviews;
     @wire(getRecord, { recordId: '$recordId', fields: AVERAGE_USER_RATING_FIELD })
     title;
+    @track detailsPage = false;
 
     get averageUserRating() { return this.title.data ? getFieldValue(this.title.data, AVERAGE_USER_RATING_FIELD) : 0 ; }
+    
 
     connectedCallback() {
-        getTitleReviews({ titleId: this.recordId})
-            .then(result => {
-                this.reviews = result;
-            })
-            .catch(error => {
-                console.error('Error occured', error);
-            });
+        if(this.recordId != ''){
+            console.log('Record Id ->' + this.recordId);
+            this.detailsPage = true;
+            getTitleReviews({ titleId: this.recordId})
+                .then(result => {
+                    this.reviews = result;
+                })
+                .catch(error => {
+                    console.error('Error occured', error);
+                });
+        } else{
+            generateUserReviews()
+             .then(result => {
+                    this.reviews = result;
+             })
+               .catch(error => {
+                  console.error('Error occured', error);
+              });
+        }
     }
 }
