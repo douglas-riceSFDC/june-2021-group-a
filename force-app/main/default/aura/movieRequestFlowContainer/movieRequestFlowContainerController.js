6e08dc794465a2a3fc1659f699cc72c363b6f846
Adam Brunner <abrunner@salesforce.com>
2021-07-14 14:02:56 -0400

    CLENHBC-635: Correct toast message logic in handleStatusChange

--- force-app/main/default/aura/movieRequestFlowContainer/movieRequestFlowContainerController.js
+++ force-app/main/default/aura/movieRequestFlowContainer/movieRequestFlowContainerController.js
@@ -8,25 +8,23 @@
 
     handleStatusChange : function (component, event, handler) {
         
-        let toastTitle, toastMessage, toastType;
+        var toastEvent = $A.get("e.force:showToast");
         
-        if (event.getParam("status" === "FINISHED")) {
-            toastTitle = "Request Submitted!"
-            toastMessage = "Your request will be processed by our Acquisitions Team."
-            toastType = "success"
-        } else if (event.getParam("status" === "ERROR")) {
-            toastTitle = "Request Submission Failed"
-            toastMessage = "An unknown error occured."
-            toastType = "error"
+        if (event.getParam("status") === "FINISHED") {
+            toastEvent.setParams({
+                "title": "Request Submitted!",
+                "message": "Your request will be processed by our Acquisitions Team.",
+                "type": "success"
+            });
+        } else if (event.getParam("status") === "ERROR") {
+            toastEvent.setParams({
+                "title": "Request Submission Failed",
+                "message": "An unknown error occured.",
+                "type": "error"
+            });
         }
         else;
 
-        let toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({
-            "title": toastTitle,
-            "message": toastMessage,
-            "type": toastType
-        });
         toastEvent.fire();
     }
 })
