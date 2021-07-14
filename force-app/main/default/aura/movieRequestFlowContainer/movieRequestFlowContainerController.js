f7bc86078e8c164c8eb4a134fcb6559992e26a0b
Adam Brunner <abrunner@salesforce.com>
2021-07-14 14:44:15 -0400

    CLENHBC-635: Convert markup to render flow in modal

--- force-app/main/default/aura/movieRequestFlowContainer/movieRequestFlowContainerController.js
+++ force-app/main/default/aura/movieRequestFlowContainer/movieRequestFlowContainerController.js
@@ -1,12 +1,15 @@
 ({
-
     handleRequestButtonClick : function (component) {
+        component.set('v.isModalOpen', true);
         let flow = component.find("movieRequest");
-        flow.startFlow("Movie_Request")
-        component.set('v.isClosed', false);
+        flow.startFlow("Movie_Request");
     },
 
-    handleStatusChange : function (component, event, handler) {
+    handleModalCloseClick : function (component) {
+        component.set('v.isModalOpen', false);
+    },
+
+    handleStatusChange : function (component, event) {
         
         var toastEvent = $A.get("e.force:showToast");
         
@@ -16,15 +19,17 @@
                 "message": "Your request will be processed by our Acquisitions Team.",
                 "type": "success"
             });
+            toastEvent.fire();
+            component.set('v.isModalOpen', false);
         } else if (event.getParam("status") === "ERROR") {
             toastEvent.setParams({
                 "title": "Request Submission Failed",
                 "message": "An unknown error occured.",
                 "type": "error"
             });
+            toastEvent.fire();
+            component.set('v.isModalOpen', false);
         }
         else;
-
-        toastEvent.fire();
     }
 })
