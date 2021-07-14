05184a0a8bfebd6a54ff2a612affe1ce095446d9
Adam Brunner <abrunner@salesforce.com>
2021-07-14 11:57:46 -0400

    CLENHBC-635: Add button to launch Movie Request Flow

--- force-app/main/default/aura/movieRequestFlowContainer/movieRequestFlowContainerController.js
+++ force-app/main/default/aura/movieRequestFlowContainer/movieRequestFlowContainerController.js
@@ -1,5 +1,32 @@
 ({
-    myAction : function(component, event, helper) {
 
+    handleRequestButtonClick : function (component) {
+        let flow = component.find("movieRequest");
+        flow.startFlow("Movie_Request")
+        component.set('v.isClosed', false);
+    },
+
+    handleStatusChange : function (component, event, handler) {
+        
+        let toastTitle, toastMessage, toastType;
+        
+        if (event.getParam("status" === "FINISHED")) {
+            toastTitle = "Request Submitted!"
+            toastMessage = "Your request will be processed by our Acquisitions Team."
+            toastType = "success"
+        } else if (event.getParam("status" === "ERROR")) {
+            toastTitle = "Request Submission Failed"
+            toastMessage = "An unknown error occured."
+            toastType = "error"
+        }
+        else;
+
+        let toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": toastTitle,
+            "message": toastMessage,
+            "type": toastType
+        });
+        toastEvent.fire();
     }
 })
